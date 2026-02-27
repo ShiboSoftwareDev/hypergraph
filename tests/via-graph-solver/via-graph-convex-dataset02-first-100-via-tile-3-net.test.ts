819e47616310da006eaa5d9bb589a927d04061a9
Abse2001 <abdaslam13.2.2001@gmail.com>
2026-02-27 20:18:52 +0200

    patch

--- tests/via-graph-solver/via-graph-convex-dataset02-first-100-via-tile-3-net.test.ts
+++ tests/via-graph-solver/via-graph-convex-dataset02-first-100-via-tile-3-net.test.ts
@@ -2,7 +2,7 @@ import { test } from "bun:test"
 import viaTile3Net from "assets/ViaGraphSolver/via-tile-3-regions.json"
 import { solveFirstHundredSamples } from "../fixtures/via-graph-convex-dataset02-100-samples.helper"
 
-test("via-graph-convex-dataset02: solve first 100 samples with via-tile-2.json", () => {
+test.skip("via-graph-convex-dataset02: solve first 100 samples with via-tile-2.json", () => {
   solveFirstHundredSamples(viaTile3Net, "via-tile.json", {
     tileHeight: 2.399,
     tileWidth: 2.408,
